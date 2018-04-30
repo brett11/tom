f4dfc3b2a037a7a144ed458e8be82d3609085907
Scott Fox <scottfox87@gmail.com>
2018-04-30 16:06:43 -0500

    update contact view with hidden field for spam filtering

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,18 +19,3 @@ class MessagesController < ApplicationController
       params.require(:message).permit(:name, :phone_number, :body)
     end
 end
-
-
-
-=begin  OLD CONTACT METHOD ... new one from Mackenzie Child video
-def create
-  @message = Message.new(message_params)
-  if @message.valid?
-    MessageMailer.contact(@message).deliver_now
-    redirect_to new_message_url
-    flash[:notice] = "Thanks!  We have received your message and will be in touch soon."
-  else
-    flash[:danger] = "There was an error in sending your message.  Please try again."
-    render :new
-  end
-end=end
