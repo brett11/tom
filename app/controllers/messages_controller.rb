8ac0f580e6db85e34e73de16dc56050c208eb05f
Scott Fox <scottfox87@gmail.com>
2018-04-30 15:51:20 -0500

    rework contact form model and controller to use mailform

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,13 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    if @message.valid?
-      MessageMailer.contact(@message).deliver_now
-      redirect_to new_message_url
-      flash[:notice] = "Thanks!  We have received your message and will be in touch soon."
-    else
-      flash[:danger] = "There was an error in sending your message.  Please try again."
-      render :new
-    end
+    @message.request = request
+      if @message.deliver
+        flash[:notice] = "Thanks!  We have received your message and will be in touch soon."
+      else
+        flash[:danger] = "There was an error in sending your message.  Please try again."
+        render :new
+      end
   end
 
   private
@@ -20,3 +19,18 @@ class MessagesController < ApplicationController
       params.require(:message).permit(:name, :phone_number, :body)
     end
 end
+
+
+
+=begin  OLD CONTACT METHOD ... new one from Mackenzie Child video
+def create
+  @message = Message.new(message_params)
+  if @message.valid?
+    MessageMailer.contact(@message).deliver_now
+    redirect_to new_message_url
+    flash[:notice] = "Thanks!  We have received your message and will be in touch soon."
+  else
+    flash[:danger] = "There was an error in sending your message.  Please try again."
+    render :new
+  end
+end=end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,14 @@
-class Message
-  include ActiveModel::Model
-  attr_accessor :name, :phone_number, :body
-  validates :name, :phone_number, presence: true
+class Message < MailForm::Base
+  attribute :name,            :validate => true
+  attribute :phone_number,    :validate => true
+  attribute :body,            :validate => true
+  attribute :nickname,        :captcha => true  #to catch spam
+
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "tomfox9235@gmail.com",
+      :from => %("#{name}" <#{phone_number}>)
+    }
+  end
 end
